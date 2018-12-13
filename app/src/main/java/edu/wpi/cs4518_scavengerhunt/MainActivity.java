c6ff5c0c917d2b0b8edf681915fe918a40d29d39
Evan Duffy <elduffy@wpi.edu>
2018-12-12 22:31:43 -0500

    Added Function For takePicture

--- app/src/main/java/edu/wpi/cs4518_scavengerhunt/MainActivity.java
+++ app/src/main/java/edu/wpi/cs4518_scavengerhunt/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.wpi.cs4518_scavengerhunt;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,12 +17,17 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static int REQUEST_IMAGE_CAPTURE = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MLOffDeviceHelper offDeviceHelper = new MLOffDeviceHelper(this);
-        Log.d("Random Item:" , offDeviceHelper.labels[(int)(Math.random()*1000)]);
+        Log.d("Random Item:", offDeviceHelper.labels[(int) (Math.random() * 1000)]);
+        String path = takePicture();
+        Log.d("Path", path);
     }
 
 
@@ -42,6 +53,33 @@ public class MainActivity extends AppCompatActivity {
 
         return f;
 
+    }
+
+    /**
+     * Function to take picture with the device camera
+     *
+     * @return The absolute path to the cached image taken
+     */
+    public String takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File picturesFolder = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File tempFile;
+        try {
+            tempFile = File.createTempFile(String.valueOf(R.string.app_name).replace(' ', '_'), ".jpg", picturesFolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Uri fileUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", tempFile);
+
+        String absoluteImagePath = tempFile.getAbsolutePath();
+
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        return absoluteImagePath;
     }
 
 }
